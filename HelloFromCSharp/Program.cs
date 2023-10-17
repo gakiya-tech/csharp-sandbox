24836d91f09e52c8d025ef2fdaa638b4d5ef1c39
Keyzo Gakiya <keyzo.gakiya@outlook.com>
2023-10-17 14:08:41 -0700

    Initial Version

---
+++ HelloFromCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string NoVowels = RemoveVowels("Hello there, amigo, you are cool");
+Console.WriteLine(NoVowels);
+
+static string RemoveVowels(string input) {
+
+    string noVow = string.Empty;
+
+    for (int i = 0; i < input.Length; i++) {
+        if (input[i] == 'a' || input[i] == 'e' || input[i] == 'í' || input[i] == 'o' || input[i] == 'u' || input[i]  == 'y')
+        {
+            continue;
+        }
+        else {
+            noVow += input[i];
+        }
+    }
+
+    return noVow;
+}
